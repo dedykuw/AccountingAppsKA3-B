0aee60feb78f8752f11cb77fc9086672ab5c6e59
Vina Juliantika.B <vinajuliantika.b@gmail.com>
2018-10-01 09:46:55 +0700

    Form Penerimaan Barang

---
+++ resources/views/formpenerimaanbarang.blade.php
@@ -0,0 +1,119 @@
+<div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+                <tr>
+                  <td>Nama Pemasok : <td><select name="lama" width="100%">
+											<option value="1">Nama Pemasok
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+				  <td>No. Pembelian : <td><input width="100%"></input></td></td>
+				  <td>Nomor PO : <td><input width="100%"></input></td></td>
+				  <td>Tanggal : <td><input type="date" class="form-control" name="" value='' width="100%"></td></td>                
+                </tr>
+				 <tr>
+                  <td>Masuk ke Gudang : <td><input type="text" class="form-control" name="" value="Head Quarter" width="100%" disabled></input></td></td>
+				  <td>Keterangan : <td><input></input></td></td>                
+                </tr>
+              </thead>
+		</table>
+		<br>
+<table style="width: 738px; height: 131px;">
+<tbody>
+<tr style="height: 6px;">
+<td style="width: 68px; height: 6px; text-align: center;">No. Barang</td>
+<td style="width: 88px; height: 6px; text-align: center;">Deskripsi Barang</td>
+<td style="width: 65px; height: 6px; text-align: center;">Di Terima</td>
+<td style="width: 65px; height: 6px; text-align: center;">Di Order</td>
+<td style="width: 68px; height: 6px; text-align: center;">Satuan</td>
+<td style="width: 65px; height: 6px; text-align: center;">Harga</td>
+<td style="width: 63px; height: 6px; text-align: center;">Disc (%)</td>
+<td style="width: 64px; height: 6px; text-align: center;">Total</td>
+<td style="width: 64px; height: 6px; text-align: center;">Pjk</td>
+<td style="width: 64px; height: 6px; text-align: center;">Job</td>
+</tr>
+<tr style="height: 30px;">
+<td style="width: 68px; height: 30px;">&nbsp;</td>
+<td style="width: 88px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 68px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 63px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+</tr>
+<tr style="height: 30px;">
+<td style="width: 68px; height: 30px;">&nbsp;</td>
+<td style="width: 88px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 68px; height: 30px;">&nbsp;</td>
+<td style="width: 65px; height: 30px;">&nbsp;</td>
+<td style="width: 63px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+<td style="width: 64px; height: 30px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+		
+		<br>
+		<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+                <tr>
+                  <td>Term of Payment 1: <td><select name="lama" width="100%">
+											<option value="1">Term of Payment
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+                </tr>
+				 <tr>
+				  <td>Bag. Pembelian : <td><select name="lama" width="100%">
+											<option value="1">Daftar Nama Karyawan
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+				                  
+                </tr>
+				<tr>
+				  <td>Term Pembayaran : <td><input></td></td>            
+                </tr>
+				<tr>
+				  <td>Credit/Debit Memo : <td><input></td></td>            
+                </tr>
+              </thead>
+		</table>
+		<br>
+		<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+				<tr>
+                  <td>Biaya Lain-Lain <td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Total Pajak <td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Total Setelah Pajak<td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Dibayar/Uang Muka<td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Saldo Terhutang<td>:</td> <td><input></input></td></td>          
+                </tr>
+              </thead>
+		</table>
+		<button href="" class="btn btn-info">Rekam</button>
+			<button href="" class="btn btn-danger">Batal</button>
+</div>              
