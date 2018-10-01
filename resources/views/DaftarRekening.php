169e54c842184af54c575eb0b0261cd5e8d4a94e
fadhilailmi16@gmail.com <fadhilailmi16@gmail.com>
2018-09-30 18:19:57 -0700

    tambah baru

---
+++ resources/views/DaftarRekening.php
@@ -0,0 +1,36 @@
+<div class="table-responsive">
+<h2> Tambah Akun Baru </h2>
+	<table class="table table-bordered" id="dataTable" width="35%" cellspacing="0">
+             <thead>
+				<tr>
+				<td>Klasifikasi <td>:</td> <td><select name="lama" width="100%">
+											<option value="1">Kas
+											<option value="2">Bank
+											<option value="3">Piutang Usaha
+											<option value="4">Piutang Non Usaha
+											<option value="5">Persediaan
+											<option value="6">Biaya dibayar dimuka
+											<option value="7">Pajak dibayar dimuka</select></td></td>
+					<td><button href="" class="btn btn-info">Alias</button></td>
+				</tr>
+				<tr>
+                  <td>Tanggal  <td>:</td><td><input type="date" class="form-control" name="" value='' width="100%"></td></td>
+				</tr>
+				<tr>
+                  <td>Keterangan <td>:</td> <td><input width="100%"></input></td></td>
+				</tr>
+				<tr>
+				<td>
+				<br>
+					<input type="radio" name=" " value="Antar">Kas/bank
+					<input type="radio" name=" " value="Ambil">Tidak aktif</td>
+				</tr>
+			 </thead>
+	</table>
+
+	<br>
+	<br><br>
+	<button href="" class="btn btn-info">Baru</button>
+			<button href="" class="btn btn-danger">Batal</button>
+			<button href="" class="btn btn-danger">Rekam</button>
+</div>
---
+++ resources/views/LaporanJurnalUmum.php
@@ -0,0 +1,92 @@
+<div class="table-responsive">
+<h2> Jurnal Umum </h2>
+	<table class="table table-bordered" id="dataTable" width="35%" cellspacing="0">
+             <thead>
+				<tr>
+                  <td>Referensi <td>:<td><input width="100%"></input></td></td>
+				</tr>
+				<tr>
+                  <td>Tanggal  <td>:</td><td><input type="date" class="form-control" name="" value='' width="100%"></td></td>
+				</tr>
+				<tr>
+                  <td>Keterangan <td>:</td> <td><input width="100%"></input></td></td>
+				</tr>
+			 </thead>
+	</table>
+
+	<table border="1" width="300">
+		</thead>	
+			<tr>
+				<td>Kode</td>
+				<td>Nama Akun</td>
+				<td>Dept.</td>
+				<td>Debit</td>
+				<td>Kredit</td>
+				<td>Job</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			</tr>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			</tr>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			</tr>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   </tr>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			</tr>
+			<tr>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			   <td><input width="100%"></input></td>
+			</tr>
+ </tbody>
+</table>
+		<thead>
+		<tr>
+			<td>Total : </td>
+		</tr>
+		<tr>
+			<td>Balance : </td>
+		</tr>
+		</thead>
+
+	<br>
+	<button href="" class="btn btn-info">Rekam</button>
+			<button href="" class="btn btn-danger">Batal</button>
+			<button href="" class="btn btn-danger">Hapus Baris</button>
+</div>
