b9b86e24dea82867e1d6bd7fb262600ea9939060
sellymln051@gmail.com <sellymln051@gmail.com>
2018-10-01 08:42:04 +0700

    kelompok 4

---
+++ resources/views/dataproduk.blade.php
@@ -0,0 +1,75 @@
+<div class="table-responsive">
+<h1>Data Produk</h1>
+		<br>
+			<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+			<thead>
+                <tr>
+                  <td>Kode Barang <td>:</td> <td><input width="100%"></input></td></td>
+				<td>Satuan Dasar: <td><select name="lama" width="100%">
+											<option value="1">Pcs
+											<option value="2">2 
+											<option value="3">3 
+											<option value="4">4 
+											<option value="5">5 
+											<option value="6">6 
+											<option value="7">7 </select></td></td>
+                </tr>               
+              </thead>
+			<thead>
+				<tr>
+                  <td>Deskripsi <td>: </td> <td><input width="100%"></input></td></td>         
+                </tr>
+				<tr>
+                  <td>Kelompok Barang <td>: </td> <td><input width="100%"></input></td></td>      
+                </tr>
+              </thead>
+              <thead>
+				 <tr>
+                  <td>Dalam Stok <td>: </td> <td><input width="50%"></input></td></td> 
+				  <td>Harga Beli Satuan : <td><input width="50%"></td></td>
+				  <td>Pajak: <td><select name="lama" width="50%">
+											<option value="1">Term of Payment
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>     
+                </tr>
+              </thead>
+			   <tr>
+                  <td>Telah dipesan ke Supplier <td>: </td> <td><input width="100%"></input></td></td> 
+				  <td>Harga Jual Satuan : <td><input width="100%"></td></td>
+				  <td>Pajak: <td><select name="lama" width="100%">
+											<option value="1">Term of Payment
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>     
+                </tr>
+				 <thead>
+				 <tr>
+                  <td>Telah dipesan Pelanggan <td>: </td> <td><input width="100%"></input></td></td> 
+				  <td>Harga Pokok Satuan : <td><input width="100%"></td></td>
+				  <td>Departmen : <td><input width="100%"></td></td>     
+                </tr>
+				
+              </thead>
+			  <thead>
+				<tr>
+                  <td>Stok Minimal <td>: </td> <td><input width="100%"></input></td></td>      
+                </tr>
+              </thead>
+			  <thead>
+                <tr>
+                  <td>Minimal Pesanan <td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Supplier Utama : <td><input width="100%"></td></td>
+                </tr>               
+              </thead>
+		</table>
+		
+		<button href="" class="btn btn-info">Rekam</button>
+			<button href="" class="btn btn-danger">Batal</button>
+</div>              
---
+++ resources/views/pemindahanbarang.blade.php
@@ -0,0 +1,100 @@
+<div class="table-responsive">
+<h1>Pemindahan Barang</h1>
+
+		<br>
+			<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+			<thead>
+				<tr>
+                  <td>Ref <td>: </td> <td><input width="100%"></input></td></td>    
+                </tr>
+				<tr>
+                  <td>Tanggal <td>: </td> <td><input type ="date" class="form-control" name="" value='' width="100%"></input></td></td>      
+                </tr>
+				<tr>
+                  <td>Keterangan <td>: </td> <td><input width="100%"></input></td></td>      
+                </tr>
+              </thead>
+             
+		</table>
+		<table border="1" width="300">
+			</thead>
+				<tr>
+					<td>Kode</td>
+					<td>Nama Barang</td>
+					<td>Jumlah</td>
+					<td>Satuan</td>
+					<td>Harga Satuan</td>
+					<td>Total</td>
+					<td>Job</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+				<tr>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+					<td><input width="100%"></input></td>
+				</tr>
+			</tbody>
+			</table>
+		
+		<button href="" class="btn btn-info">Rekam</button>
+			<button href="" class="btn btn-danger">Batal</button>
+</div>              
