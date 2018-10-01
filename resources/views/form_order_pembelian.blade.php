9c84742f0b8bbc5d31821a1b068c0ba500211dd3
Vina Juliantika.B <vinajuliantika.b@gmail.com>
2018-10-01 09:58:03 +0700

    baru

---
+++ resources/views/form_order_pembelian.blade.php
@@ -0,0 +1,104 @@
+<link href="{asset('css/app.css')}" rel="stylesheet"/>
+<div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+                <tr>
+                  <td>Nama Pemasok : <td><select name="lama" width="100%">
+											<option value="1">Nama Pemasok
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+				  <td>Nomor PO : <td><input width="100%"></input></td></td>
+				  <td>Tanggal : <td><input type="date" class="form-control" name="" value='' width="100%"></td></td>                
+                </tr>
+				 <tr>
+                  <td>Masuk ke Gudang : <td><input type="text" class="form-control" name="" value="Head Quarter" width="100%" disabled></input></td></td>
+				  <td>Penawaran No. : <td><input width="100%"></td></td>
+				  <td>Keterangan : <td><input></input></td></td>                
+                </tr>
+              </thead>
+		</table>
+		<br>
+		 <table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+				<tr>
+                  <td>No. Barang <td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Harga <td>:</td> <td><input width="100%"></td></td>            
+                </tr>
+				<tr>
+                  <td>Deskripsi <td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Disc (%.) <td>:</td> <td><input width="100%"></td></td>            
+                </tr>
+				<tr>
+                  <td>Di Order <td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Total <td>:</td> <td><input width="100%"></td></td>            
+                </tr>
+				<tr>
+                  <td>Di Terima <td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Pjk <td>:</td> <td><input width="100%"></td></td>            
+                </tr>
+				<tr>
+                  <td>Satuan<td>:</td> <td><input width="100%"></input></td></td>
+				  <td>Job <td>:</td> <td><input width="100%"></td></td>            
+                </tr>
+              </thead>
+		</table>
+		<br>
+		<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+                <tr>
+                  <td>Term of Payment 1: <td><select name="lama" width="100%">
+											<option value="1">Term of Payment
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+				<td>Term of Payment 2: <td><select name="lama" width="100%">
+											<option value="1">Term of Payment
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+                </tr>
+				 <tr>
+				  <td>Tgl. Pengantaran : <td><input type="date" class="form-control" name="" value='' width="100%"></td></td>
+				  <td>Bag. Pembelian : <td><select name="lama" width="100%">
+											<option value="1">Daftar Nama Karyawan
+											<option value="2">2 Hari
+											<option value="3">3 Hari
+											<option value="4">4 Hari
+											<option value="5">5 Hari
+											<option value="6">6 Hari
+											<option value="7">7 Hari</select></td></td>
+				                  
+                </tr>
+				<tr>
+				  <td>Term Pembayaran : <td><input></td></td>
+				  <td>Catatan Atas Orde : <td><input width="100%"></td></td>              
+                </tr>
+              </thead>
+		</table>
+		<br>
+		<table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
+              <thead>
+				<tr>
+                  <td>Biaya Lain-Lain <td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Total Pajak <td>:</td> <td><input></input></td></td>          
+                </tr>
+				<tr>
+                  <td>Total Setelah Pajak<td>:</td> <td><input></input></td></td>          
+                </tr>
+              </thead>
+		</table>
+		<button href="" class="btn btn-info">Rekam</button>
+			<button href="" class="btn btn-danger">Batal</button>
+</div>              
