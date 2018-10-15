a9fa74a6fc5e77c737fa84055a8b839f9f09dd1d
melisatricahyaningsih <melisatricahyaningsih@gmail.com>
2018-10-15 07:09:03 +0700

    tambahan

---
+++ resources/views/form_order_penjualan.php
@@ -0,0 +1,125 @@
+<link href="{asset('css/app.css')}" rel="stylesheet"/>
+<table>
+
+<tr>
+  <td>Nama Pelanggan</td>
+  <td>:</td>
+  <td> <input type="text" name="namapelanggan" maxlength="23"> </td>
+</tr>
+
+<tr>
+  <td>No SO</td>
+  <td>:</td>
+  <td> <input type="text" name="noso"> </td> 
+</tr>
+
+<tr>
+	<td>Penawaran NO</td>
+	<td>:</td>
+	<td> <input type="text" name="penawaaran"> </td>
+</tr>
+
+<tr>
+	<td>Tanggal</td>
+	<td>:<td>
+	<input type="date" name="ttl">
+</tr>
+
+<tr>
+	<td>Keterangan</td>
+	<td>:</td>
+	<td> <input type="text" name="ket"> </td>
+</tr>
+
+<table border="1">
+<tr>
+<td>No.Barang</td>
+<td>Deskripsi Barang</td>
+<td>Di Order</td>
+<td>Di Kirim</td>
+<td>Satuan</td>
+<td>Harga</td>
+<td>Disc(%)</td>
+<td>Total</td>
+<td>Pjk</td>
+<td>Job</td>
+</tr>
+
+<tr>
+	<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+
+<tr>
+	<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+
+<tr>
+	<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+</table>
+
+
+<tr>
+	<td>Term of Sales 1</td>
+	<td>:</td>
+	<td> <input type="text" name="term1"> </td>
+</tr>
+
+<tr>
+	<td>Term of Sales 2</td>
+	<td>:</td>
+	<td> <input type="text" name="term2"> </td>
+</tr>
+
+<tr>
+	<td>Tanggal Pengantaran</td>
+	<td>:<td>
+	<input type="date" name="ttl1">
+</tr>
+
+<tr>
+	<td>Term Pembayaran</td>
+	<td>:</td>
+	<td> <input type="text" name="termbayar"> </td>
+</tr>
+
+<tr>
+	<td>Salesman</td>
+	<td>:</td>
+	<td> <input type="text" name="ortuibu"> </td>
+</tr>
+
+<tr>
+	<td>Catatan Atas Order 1</td>
+	<td>:</td>
+	<td> <input type="text" name="ortuibu"> </td>
+</tr>
+
+</table>             
---
+++ resources/views/form_pegiriman_barang.php
@@ -0,0 +1,96 @@
+<link href="{asset('css/app.css')}" rel="stylesheet"/>
+<link href="{aset('css/app.css')}" rel="stylesheet"/>
+<table>
+
+<tr>
+  <td>Nama Pelanggan</td>
+  <td>:</td>
+  <td> <input type="text" name="namapelanggan" maxlength="23"> </td>
+</tr>
+
+<tr>
+  <td>No DO</td>
+  <td>:</td>
+  <td> <input type="text" name="nosdo"> </td> 
+</tr>
+
+<tr>
+	<td>NO SO</td>
+	<td>:</td>
+	<td> <input type="text" name="noso"> </td>
+</tr>
+
+<tr>
+	<td>Keluar dari Gudang</td>
+	<td>:</td>
+	<td> <input type="text" name="noso"> </td>
+</tr>
+
+<tr>
+	<td>Keterangan</td>
+	<td>:</td>
+	<td> <input type="text" name="ket"> </td>
+</tr>
+
+<table border="1">
+<tr>
+<td>No.Barang</td>
+<td>Deskripsi Barang</td>
+<td>Di Order</td>
+<td>Di Kirim</td>
+<td>Satuan</td>
+<td>Harga</td>
+<td>Disc(%)</td>
+<td>Total</td>
+<td>Pjk</td>
+<td>Job</td>
+</tr>
+
+<tr>
+		<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+
+<tr>
+	<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+
+<tr>
+	<td> </td>
+		<td> </td>
+			<td> </td>
+				<td> </td>
+					<td> </td>
+						<td> </td>
+							<td> </td>
+								<td> </td>
+									<td> </td>
+										<td> </td>
+</tr>
+</table>
+
+
+<tr>
+	<td>Term Pembayaran</td>
+	<td>:</td>
+	<td> <input type="text" name="termbayar"> </td>
+</tr>
+
+</table>            
