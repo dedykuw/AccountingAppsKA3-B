5c0e07d46534bc76dcc9efd79a03d74d9ebd4800
Amin Rais <aminrais7809@gmail.com>
2018-10-29 20:34:16 +0700

    kas dan bank

---
+++ resources/views/kasmasuk.blade.php
@@ -0,0 +1,79 @@
+<link href ="{{ asset('css/app.css')}}" rel="stylesheet"/>
+<p><strong>Kas Masuk</strong></p>
+
+                      
+              <input class="form-control" type="text" style="float: right" placeholder="Search">
+        
+        
+        
+<p style="text-align: right;"><strong><a href="#top" target="_blank" rel="noopener" title="Mata Uang">Mata Uang</a>&nbsp; &nbsp;<a href="#top" target="_blank" rel="noopener" title="Update">Update</a>&nbsp; <a href="#top" title="Filter" target="_blank" rel="noopener">&nbsp;Filter</a></strong></p>
+<table border="1" style="border-collapse: collapse; width: 100%;">
+<tbody>
+
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px; text-align: center;">Tanggal</td>
+<td style="width: 18.726%; height: 21px; text-align: center;">Referensi</td>
+<td style="width: 22.3886%; height: 21px; text-align: center;">Disetor Oleh</td>
+<td style="width: 28.5987%; height: 21px; text-align: center;">Keterangan</td>
+<td style="width: 16.6561%; height: 21px; text-align: center;">Nilai</td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+<tr style="height: 21px;">
+<td style="width: 13.6306%; height: 21px;"></td>
+<td style="width: 18.726%; height: 21px;"></td>
+<td style="width: 22.3886%; height: 21px;"></td>
+<td style="width: 28.5987%; height: 21px;"></td>
+<td style="width: 16.6561%; height: 21px;"></td>
+</tr>
+</tbody>
+</table>
+<p>
+        <button type="button" class="btn btn-lg btn-default">Baru</button>
+        <button type="button" class="btn btn-lg btn-primary">Hapus</button>
+        <button type="button" class="btn btn-lg btn-success" style= "float:right"  >Tutup</button>
+        <button type="button" class="btn btn-lg btn-info" style= "float:right ">Cetak</button>
+        <button type="button" class="btn btn-lg btn-warning" style= "float:right">Perincian</button>
+
+      </p>
+
